c22ba29f8803c8217de5d0f2527b524c0e803936
Yau, Ding MF <ding.mf.yau@pccw.com>
2021-08-06 10:13:57 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [messages, setMessages] = React.useState([]);
+  const resetMessages = React.useCallback(() => {
+    setMessages([]);
+  }, []);
+
+  const addMessage = React.useCallback((newMessage) => {
+    setMessages((prev) => prev.concat(newMessage));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header resetMessages={resetMessages} />
+      <Content messages={messages} addMessage={addMessage} />
     </div>
   );
 }
 
-export default App;
+const Header = ({ resetMessages }) => {
+  return (
+    <div className="header-containenr ">
+      <div>Header</div>
+      <input type="button" value={"R"} onClick={resetMessages} />
+    </div>
+  );
+};
+
+const Content = ({ messages, addMessage }) => {
+  return (
+    <div className="content-container">
+      <MessageList messages={messages} />
+      <InputBar addMessage={addMessage} />
+    </div>
+  );
+};
+
+const MessageList = ({ messages }) => {
+  return (
+    <div className="message-list-container">
+      {messages.map((message) => (
+        <MessageItem
+          className="input-bar-container"
+          key={message.id}
+          message={message}
+        />
+      ))}
+    </div>
+  );
+};
+
+const MessageItem = ({ message }) => {
+  return <div className="message-item-container">{message.text}</div>;
+};
+
+const InputBar = ({ addMessage }) => {
+  const textRef = React.useRef();
+
+  const sendMessage = React.useCallback(() => {
+    if (!textRef.current.value) return;
+    const newMessage = {
+      id: new Date().getTime(),
+      text: textRef.current.value
+    };
+    addMessage(newMessage);
+    textRef.current.value = "";
+    textRef.current.focus();
+  }, [addMessage]);
+
+  return (
+    <div className="input-bar-container">
+      <input type="text" ref={textRef} />
+      <input type="button" value={">"} onClick={sendMessage} />
+    </div>
+  );
+};
