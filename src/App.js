0202de76a31ac606a87c52413dc7cfc514a6d774
Yau, Ding MF <ding.mf.yau@pccw.com>
2021-08-09 11:02:46 +0800

    update author and date

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
-import React from "react";
-import "./styles.css";
+import React from 'react'
+import './styles.css'
 
 export default function App() {
-  const [messages, setMessages] = React.useState([]);
+  const [userName, setUserName] = React.useState('')
+  const [messages, setMessages] = React.useState([])
   const resetMessages = React.useCallback(() => {
-    setMessages([]);
-  }, []);
+    setMessages([])
+  }, [])
 
   const addMessage = React.useCallback((newMessage) => {
-    setMessages((prev) => prev.concat(newMessage));
-  }, []);
+    setMessages((prev) => prev.concat(newMessage))
+  }, [])
 
   return (
     <div className="app-container">
-      <Header resetMessages={resetMessages} />
-      <Content messages={messages} addMessage={addMessage} />
+      {!userName && <AskNamePage setUserName={setUserName} />}
+      {userName && (
+        <>
+          <Header resetMessages={resetMessages} />
+          <Content
+            messages={messages}
+            addMessage={addMessage}
+            userName={userName}
+          />
+        </>
+      )}
     </div>
-  );
+  )
+}
+
+const AskNamePage = ({ setUserName }) => {
+  const [text, setText] = React.useState('')
+
+  const sendMessage = () => {
+    if (!text) return
+    setUserName(text)
+    setText('')
+  }
+
+  const onChange = (e) => {
+    setText(e.target.value)
+  }
+  return (
+    <div className="ask-name-page-container">
+      <input type="text" value={text} onChange={onChange} />
+      <input type="button" value={'Start'} onClick={sendMessage} />
+    </div>
+  )
 }
 
 const Header = ({ resetMessages }) => {
   return (
-    <div className="header-containenr ">
+    <div className="header-container ">
       <div>Header</div>
-      <input type="button" value={"R"} onClick={resetMessages} />
+      <input type="button" value={'R'} onClick={resetMessages} />
     </div>
-  );
-};
+  )
+}
 
-const Content = ({ messages, addMessage }) => {
+const Content = ({ messages, addMessage, userName }) => {
   return (
     <div className="content-container">
       <MessageList messages={messages} />
-      <InputBar addMessage={addMessage} />
+      <InputBar addMessage={addMessage} userName={userName} />
     </div>
-  );
-};
+  )
+}
 
 const MessageList = ({ messages }) => {
   return (
@@ -48,31 +78,54 @@ const MessageList = ({ messages }) => {
         />
       ))}
     </div>
-  );
-};
+  )
+}
 
 const MessageItem = ({ message }) => {
-  return <div className="message-item-container">{message.text}</div>;
-};
+  return (
+    <div className="message-item-container">
+      <MessageAuthor author={message.author} />
+      <MessageContent text={message.text} />
+      <MessageDate date={message.date} />
+    </div>
+  )
+}
 
-const InputBar = ({ addMessage }) => {
-  const textRef = React.useRef();
+const MessageContent = ({ text }) => {
+  return <div className="message-item-content-container">{text}</div>
+}
 
-  const sendMessage = React.useCallback(() => {
-    if (!textRef.current.value) return;
+const MessageAuthor = ({ author }) => {
+  return <div className="message-item-author">{author}</div>
+}
+
+const MessageDate = ({ date }) => {
+  return <div className="message-item-date">{date}</div>
+}
+
+const InputBar = ({ addMessage, userName }) => {
+  const [text, setText] = React.useState('')
+
+  const sendMessage = () => {
+    if (!text) return
     const newMessage = {
       id: new Date().getTime(),
-      text: textRef.current.value
-    };
-    addMessage(newMessage);
-    textRef.current.value = "";
-    textRef.current.focus();
-  }, [addMessage]);
+      text: text,
+      author: userName,
+      date: new Date().toISOString()
+    }
+    addMessage(newMessage)
+    setText('')
+  }
+
+  const onChange = (e) => {
+    setText(e.target.value)
+  }
 
   return (
     <div className="input-bar-container">
-      <input type="text" ref={textRef} />
-      <input type="button" value={">"} onClick={sendMessage} />
+      <input type="text" value={text} onChange={onChange} />
+      <input type="button" value={'>'} onClick={sendMessage} />
     </div>
-  );
-};
+  )
+}
